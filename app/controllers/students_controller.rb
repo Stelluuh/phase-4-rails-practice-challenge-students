f6af55bd49edb2c8942e0eb4cfef1c483c2c61f0
stella <sftamesis13@gmail.com>
2023-03-30 13:50:05 -0700

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,43 +7,42 @@ class StudentsController < ApplicationController
     end
 
     def show
-        student = find_students
+    student = find_student
         render json: student, status: :ok
     end
 
     def create
         new_student = Student.create!(student_params)
-        render json: student, status: :created
+        render json: new_student, status: :created
     end
 
     def update
-        student = find_students
+        student = find_student
         student.update!(student_params)
         render json: student, status: :accepted
     end
 
     def destroy
-        student = find_students
+        student = find_student
         student.destroy
         head :no_content
     end
 
+    private
 
-        private
-
-    def find_students
+    def find_student
         Student.find(params[:id])
     end
 
     def student_params
-        params.permit(:name, :major, :age)
+        params.permit(:name, :major, :age, :instructor_id)
     end
 
     def render_not_found_response
-        render json: { error: "Student not found"}, status: :not_found
+        render json: { error: "Student not found" }, status: :not_found
     end
 
-    def render_unprocessable_entity_response invalid
-        render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    def render_unprocessable_entity_response invalid 
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
